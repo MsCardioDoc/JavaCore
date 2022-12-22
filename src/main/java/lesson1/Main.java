2fd280f4612d162d82c1257f27f340772af69a9b
MsCardioDoc <Isi-2011@mail.ru>
2022-12-22 13:05:02 +0300

    Initial commit

---
+++ src/main/java/lesson1/Main.java
@@ -0,0 +1,12 @@
+package lesson1;
+
+public class Main {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat("Albus", "White", 1);
+        Cat cat2 = new Cat("Anton", "Black", 3);
+        Cat cat3 = new Cat("Marc", "Blue", 4);
+
+    }
+
+
+}
